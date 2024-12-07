7b96d8c90d5c60ce43d13df2ec2656374a4436f0
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-12-07 19:41:27 +0400

    requirements

--- recipes/recipes_app/models.py
+++ recipes/recipes_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=400)
+    ingredient = models.CharField(max_length=1000)
+    time = models.IntegerField()
+    process = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.name
