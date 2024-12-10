17aa250db5f9997129e73143d28142ae6ed3aaaf
Gvn <122465852+usarnameMe@users.noreply.github.com>
2024-12-10 17:21:03 +0400

    fixed DRF

--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -46,10 +46,10 @@ LOGGING = {
 ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
+    'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ),
+    ],
 }
 
 # Application definition
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'recipes_app',
 ]
 
@@ -85,12 +86,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Add any custom template directories
-        'APP_DIRS': True,  # Ensure app templates are discovered
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # Required for DRF
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
